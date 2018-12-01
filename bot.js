21f5fa854ea89c6b5d527915b7bf6de5e202af4f
ana7mar <45071917+ana7mar@users.noreply.github.com>
2018-12-02 04:52:32 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,16 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix = "1";
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);  
+  console.log(`Logged in as ${client.user.tag}!`);  
 });
 
 client.on('ready', async() => {
 var server = "514500934009618433"; // ايدي السررفر
 var channel = "514500934529974303";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , ')
+    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream ,  Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
 })
 
@@ -31,7 +32,6 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
 
  
 client.login(process.env.BOT_TOKEN);
-
 
 
  
